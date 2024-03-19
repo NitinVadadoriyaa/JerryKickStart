18061e5e45ad8358bbde6dfd0f7a43f532e837e4
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-03-19 18:28:05 +0530

    add build script for hosting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2442e2077ede78bfde19";
+/******/ 	var hotCurrentHash = "0a06db24971a3c1d7451";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
