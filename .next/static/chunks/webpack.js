bbafef0a0cbdf18e88854884efeb19146e04f1dc
NitinVadadoriya <vadadoriyanitin55@gmail.com>
2024-05-02 19:58:28 +0530

    badded!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8bd4623f16ada03bde41";
+/******/ 	var hotCurrentHash = "64f88331c32ecef7a161";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
